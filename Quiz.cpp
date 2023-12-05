91acbdbe518f886ddf416d96a2c2e25d212134c3
Matgoh <matthewgoh2003@gmail.com>
2023-12-04 17:16:55 -0700

    Added all music paths to map

--- Quiz.cpp
+++ Quiz.cpp
@@ -8,6 +8,7 @@
 #include "mainwindow.h"
 #include <QMediaPlayer>
 #include<QRandomGenerator>
+#include<map>
 
 Quiz::Quiz(MainWindow* mainWindow,QWidget *parent)
     : QWidget(parent)
@@ -45,6 +46,30 @@ Quiz::Quiz(MainWindow* mainWindow,QWidget *parent)
     musicAud = new QAudioOutput;
     musicPlayer->setAudioOutput(musicAud);
     musicAud->setVolume(60);
+
+    std::map<QString, int> musicPaths;
+    musicPaths = {{":/2Pac_CaliforniaLove.mp3", 1990}, {":/BennyGoodman_DontBeThatWay.mp3", 1930},
+                   {":/Biggie_MoMoneyMoProblems.mp3", 1990}, {":/BillieHoliday_GodBlessTheChild.mp3", 1930},
+                   {":/BingCrosby_ISurrenderDear.mp3", 1930}, {":/BobDylan_LikeARollingStone.mp3", 1960},
+                   {":/BobMarley_CouldYouBeLoved.mp3", 1970}, {":/BruceSpringsteen_BornToRun.mp3", 1980},
+                   {":/Chuck_Berry_-_Johnny_B_Goode.mp3", 1950}, {":/DaftPunk_OneMoreTime.mp3", 2000},
+                   {":/DukeEllington_Caravan.mp3", 1930}, {":/EdithPiaf_LaVieEnRose.mp3", 1940},
+                   {":/EllaFitzgerald_DreamALittleDreamOfMe.mp3", 1940}, {":/ElvisPresley_HoundDog.mp3", 1950},
+                   {":/FrankSinatra_FlyMeToTheMoon.mp3", 1940}, {":/GlenMiller_IntheMood.mp3", 1930},
+                   {":/GloriaGaynor_IWillSurvive.mp3", 1970}, {":/JamesBrown_IGotYou(IFeelGood).mp3", 1960},
+                   {":/JellyRollMorton_HonkyTonkBlues.mp3", 1920}, {":/JimiHendrix_PurpleHaze.mp3", 1960},
+                   {":/JohnnyCash_IWalkTheLine.mp3", 1950}, {":/KingOliver_DoctorJazz.mp3", 1920},
+                   {":/LadyGaga_BadRomance.mp3", 2000}, {":/LedZeppelin_ImmigrantSong.mp3", 1970},
+                   {":/LittleRichard_TuttiFruti.mp3", 1950}, {":/Louis Armstrong - What A Wonderful World.mp3", 1920},
+                   {":/Madonna_MaterialGirl.mp3", 1980}, {":/MichaelJacson_BeatIt.mp3", 1980},
+                   {":/N.K.COLE_L.O.V.E.mp3", 1940}, {":/Nirvana_SmellsLikeTeenSpirit.mp3", 1990},
+                   {":/Oasis_DontLookBackInAnger.mp3", 1990}, {":/OutKast_HeyYa.mp3", 2000},
+                   {":/Prince_Kiss.mp3", 1980}, {":/Ramones_BlitzkriegBop.mp3", 1970},
+                   {":/SidneyBechet_WildCatBlues.mp3", 1920}, {":/StevieWonder_SirDuke.mp3", 1970},
+                   {":/TheBeatles_HereComesTheSun.mp3", 1960}, {":/TheEverlyBrothers_AllIHavetoDoIsDream.mp3", 1950},
+                   {":/TheSupremes_StopintheNameofLove.mp3", 1960}, {":/ThroughTheWire.mp3", 2000},
+                   {":/VeraLynn_We_llMeetAgain.mp3", 1940}, {":/WhiteStripes_SevenNationArmy.mp3", 2000},
+                   {":/WillieSmith_EchoesOfSpring.mp3", 1920}};
 
     // Display random question
     showNextQuestion();
