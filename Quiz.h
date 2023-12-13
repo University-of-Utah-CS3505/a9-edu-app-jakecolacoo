65bdb7fb347bd33238b5779bf89525c5433f872f
jakecolacoo <Jakecrane123111@gmail.com>
2023-12-12 23:30:46 -0700

    Removed redundant code and added documentation.

--- Quiz.h
+++ Quiz.h
@@ -5,9 +5,6 @@
 #include <QtMultiMedia/QtMultiMedia>
 
 namespace Ui {
-
-
-
 class Quiz;
 }
 
@@ -18,40 +15,97 @@ class Quiz : public QWidget
     Q_OBJECT
 
 public:
+    /**
+     * @brief Construct a new Quiz object
+     *
+     * @param mainWindow Pointer to the MainWindow object
+     * @param parent Pointer to the parent QWidget object (default is nullptr)
+     */
     explicit Quiz(MainWindow* mainWindow, QWidget *parent = nullptr);
+
+    /**
+     * @brief Destroy the Quiz object
+     */
     ~Quiz();
 
 private slots:
+    /**
+     * @brief Slot for handling push button click event
+     */
     void on_pushButton_clicked();
+
+    /**
+     * @brief Slot for handling era button 1 click event
+     */
     void on_eraButton1_clicked();
+
+    /**
+     * @brief Slot for handling era button 2 click event
+     */
     void on_eraButton2_clicked();
+
+    /**
+     * @brief Slot for handling era button 3 click event
+     */
     void on_eraButton3_clicked();
+
+    /**
+     * @brief Slot for handling era button 4 click event
+     */
     void on_eraButton4_clicked();
+
+    /**
+     * @brief Slot for handling retry button click event
+     */
     void on_retryButton_clicked();
 
-public slots:
-
 private:
-    Ui::Quiz *ui;
-    MainWindow* m_mainWindow;
-    QMediaPlayer *musicPlayer;
-    QAudioOutput *musicAud;
+    Ui::Quiz *ui; ///< Pointer to the UI Quiz object
+    MainWindow* m_mainWindow; ///< Pointer to the MainWindow object
+    QMediaPlayer *musicPlayer; ///< Pointer to the QMediaPlayer object
+    QAudioOutput *musicAud; ///< Pointer to the QAudioOutput object
 
+    /**
+     * @brief Set up the quiz
+     */
     void setUpQuiz();
-    void playMusic();
-    void setUpButtons();
-    void setOtherButtons(int button1, int button2, int button3);
-    void gameOver();
-    std::map<QByteArray, int> musicPaths;
-    std::vector<QByteArray> vecOfPaths;
 
-    int questionNumber;
+    /**
+     * @brief Play the music
+     */
+    void playMusic();
+
+    /**
+     * @brief Set up the buttons
+     */
+    void setUpButtons();
+
+    /**
+     * @brief Set other buttons
+     *
+     * @param button1
+     * @param button2
+     * @param button3
+     */
+    void setOtherButtons(int button1, int button2, int button3);
+
+    /**
+     * @brief End the game
+     */
+    void gameOver();
+
+    std::map<QByteArray, int> musicPaths; ///< Map of music paths
+    std::vector<QByteArray> vecOfPaths; ///< Vector of paths
+
+    int questionNumber; ///< Current question number
     int amountCorrect = 0;
     int amountIncorrect = 0;
     int correctEra;
     int correctButton;
-
 signals:
+    /**
+     * @brief Signal to notify that main window should come back
+     */
     void mainWindowComeBack();
 };
 
--- stagecreate.h
+++ stagecreate.h
@@ -3,41 +3,78 @@
 
 #include <QWidget>
 
+/**
+ * @class stageCreate
+ * @brief The stageCreate class inherits from QWidget and represents a custom widget for creating stages.
+ */
 class stageCreate : public QWidget
 {
     Q_OBJECT
 public:
+    /**
+     * @brief stageCreate Constructor.
+     * @param parent The parent widget, null pointer by default.
+     */
     explicit stageCreate(QWidget *parent = nullptr);
 
 private:
-    //sprite paths
-    QStringList imagePaths;
-    //Names of musication
-    QStringList names;
-    //5 music path
+    QStringList imagePaths; ///< Paths to the sprite images.
+
+    QStringList names; ///< Names of the musications.
+
+    ///v Paths to the music files.
     QByteArray m1;
     QByteArray m2;
     QByteArray m3;
     QByteArray m4;
     QByteArray m5;
 
-    // 5 informational paths
+    ///v Paths for the artists information files.
     QString artist1Info;
     QString artist2Info;
     QString artist3Info;
     QString artist4Info;
     QString artist5Info;
-    QString decadeInfo;
 
+    QString decadeInfo; ///< Path to file for information about the decade.
 
 public slots:
+    /**
+     * @brief Sets the era.
+     * @param era The era to set.
+     */
     void setEra(int era);
+
+    /**
+     * @brief Plays the music at the given index.
+     * @param i The index of the music to play.
+     */
     void play(int i);
+
 signals:
+    /**
+     * @brief Signal to play the music at the given path.
+     * @param path The path of the music to play.
+     */
     void playMusic(QByteArray path);
+
+    /**
+     * @brief Signal to stop the music.
+     */
     void stopMusic();
 
+    /**
+     * @brief Sends the file path for the info bar.
+     * @param filePath The file path to send.
+     */
     void sendFileForInfoBar(QString filePath);
+
+    /**
+     * @brief Sends information for the info bar.
+     * @param image The image to send.
+     * @param names The names to send.
+     * @param decadeNum The decade number to send.
+     */
     void sendInfor(QStringList image, QStringList names, int decadeNum);
 };
 
